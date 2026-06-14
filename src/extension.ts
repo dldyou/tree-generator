4291595e01819f5fe682ae7405747e82d52a37e8
dldyou <chan12_yun@naver.com>
2026-06-15 01:56:00 +0900

    ✨ feat: implement command to generate and copy directory tree to clipboard

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,46 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { scanDirectory } from './scanner';
+import { generateTreeString } from './treeGenerator';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "tree-generator" is now active!');
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('Congratulations, your extension "tree-generator" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('tree-generator.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from tree-generator!');
-	});
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with registerCommand
+    // The commandId parameter must match the command field in package.json
+    const disposable = vscode.commands.registerCommand('tree-generator.generateTree', async () => {
+        const workspaceFolders = vscode.workspace.workspaceFolders;
 
-	context.subscriptions.push(disposable);
+        if (!workspaceFolders || workspaceFolders.length === 0) {
+            vscode.window.showErrorMessage('No workspace folder is open.');
+            return;
+        }
+
+        const rootPath = workspaceFolders[0].uri.fsPath;
+
+        try {
+            const tree = await scanDirectory(rootPath);
+            const treeString = generateTreeString(tree);
+
+            await vscode.env.clipboard.writeText(treeString);
+
+            vscode.window.showInformationMessage('Directory tree copied to clipboard!');
+        } catch (error) {
+            vscode.window.showErrorMessage(
+                `Failed to generate project tree: ${String(error)}`
+            );
+        }
+    });
+
+    context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
